7ec6b3e9181405b9457a34dd357b59229f779573
Anish Sharma <aneeshsharma15@outlook.com>
2021-01-11 19:30:07 +0530

    Updates

---
+++ precache-manifest.2c46b82491f6af58bf80d162c6b2cb8b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a2433960276ba16550f025bb928f65",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "052f91bd2678b87904b1",
+    "url": "/MyPortfolio/static/css/2.00deddd5.chunk.css"
+  },
+  {
+    "revision": "b9b09df5386e83b1e23a",
+    "url": "/MyPortfolio/static/css/main.33a03feb.chunk.css"
+  },
+  {
+    "revision": "052f91bd2678b87904b1",
+    "url": "/MyPortfolio/static/js/2.f8ef2dba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyPortfolio/static/js/2.f8ef2dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b09df5386e83b1e23a",
+    "url": "/MyPortfolio/static/js/main.dccd9b3c.chunk.js"
+  },
+  {
+    "revision": "0d8bee11d1469486e213",
+    "url": "/MyPortfolio/static/js/runtime-main.7e73d9a0.js"
+  },
+  {
+    "revision": "8daf0546ac7662756f96d34988f75b4d",
+    "url": "/MyPortfolio/static/media/LEMONMILK-Bold.8daf0546.otf"
+  },
+  {
+    "revision": "5d474fd2ebc6ef77c9bf3219391bed9c",
+    "url": "/MyPortfolio/static/media/coolvetica-rg.5d474fd2.ttf"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/MyPortfolio/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/MyPortfolio/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/MyPortfolio/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/MyPortfolio/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/MyPortfolio/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/MyPortfolio/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/MyPortfolio/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/MyPortfolio/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/MyPortfolio/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/MyPortfolio/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/MyPortfolio/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/MyPortfolio/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/MyPortfolio/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/MyPortfolio/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/MyPortfolio/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "cb176140f9d33bb90c8149dc51cc88fb",
+    "url": "/MyPortfolio/static/media/landing-bg.cb176140.jpg"
+  }
+]);
